7bf5d3b00e924277a5e638cd9537934bb9f1af29
DipuBarua <you@example.com>
2023-09-15 17:29:28 +0600

    first commit and installed tailwind

---
+++ README.md
@@ -0,0 +1,10 @@
+# QA.
+
+1.Add at least 3 Project features
+Ans: 
+.
+.
+.
+
+2.Discuss how you managed the state in your assignment project.
+Ans:
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className=' text-2xl text-red-600'>Course Registration</h1>
+
+    </>
+  )
+}
+
+export default App
